4dc1e67e55ae82c54e2db22dbac66f7bbf3a6c80
Pablo Magaz <pmagaz@atsistemas.com>
2015-06-05 19:26:41 +0200

    New methods: findElements, findLastElement, addTextToElement and addBr

--- dynamic-form.js
+++ dynamic-form.js
@@ -17,27 +17,51 @@ Polymer({
 
 
     appendElement: function(formElement){
-      var container = this.$.form;
       var elementType = formElement.polymerElement.parent;
       var newElement = document.createElement(formElement.polymerElement.child);
-      return Polymer.dom(container).appendChild(newElement);
+      return Polymer.dom(this.$.form).appendChild(newElement);
+    },
+
+    findElements: function(elements){
+      var arrItems = Polymer.dom(this.$.form).querySelectorAll(elements);
+      return arrItems;
+    },
+
+    findLastElement: function(element){
+      var arrElements = this.findElements(element);
+      return arrElements[arrElements.length -1];
+    },
+
+    addTextToElement: function(text){
+      var span = document.createElement('span');
+      Polymer.dom(span).innerHTML = ' ' + text;
+      return Polymer.dom(this.$.form).appendChild(span);
+    },
+
+    addBr: function(){
+      var br = document.createElement('br');
+      return Polymer.dom(this.$.form).appendChild(br);
     },
 
     paperInputHandler: function(formElement){
       var newElement = this.appendElement(formElement);
       newElement.label = formElement.label;
       newElement.name = formElement.inputName;
+      this.addBr();
       return newElement;
     },
 
     paperRadioHandler: function(formElement){
       var options = formElement.options;
+      var elementType = formElement.polymerElement.parent;
       var arrElement = [];
       var i = 0, l = options.length;
       for(i;i<l;i++){
         var newElement = this.appendElement(formElement);
         newElement.name = formElement.inputName;
         arrElement.push(newElement);
+        this.addTextToElement(formElement.options[i].name);
+        this.addBr();
       }
       return arrElement;
     },
@@ -46,6 +70,9 @@ Polymer({
       switch(formElement.polymerElement.parent){
         case 'paper-input':
           this.paperInputHandler(formElement);
+          break;
+        case 'paper-checkbox':
+          this.paperRadioHandler(formElement);
           break;
         case 'paper-radio-button':
           this.paperRadioHandler(formElement);
