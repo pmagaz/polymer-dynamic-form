bea46fe17a06d7cfd1f2c3885d47a055770a0902
Pablo Magaz <pmagaz@atsistemas.com>
2015-06-06 23:21:05 +0200

    Improving importComponent using closures

--- dynamic-form.js
+++ dynamic-form.js
@@ -6,6 +6,7 @@ Polymer({
       
       data : {
         type: Object,
+        notify: false,
         observer: 'dataObserver'
       },
 
@@ -15,78 +16,100 @@ Polymer({
       } 
     },
 
-
-    appendElement: function(formElement){
-      var elementType = formElement.polymerElement.parent;
-      var newElement = document.createElement(formElement.polymerElement.child);
-      return Polymer.dom(this.$.form).appendChild(newElement);
+    appendElement: function(node, element){
+      var newElement = document.createElement(element);
+      return Polymer.dom(node).appendChild(newElement);
     },
 
-    findElements: function(elements){
-      var arrItems = Polymer.dom(this.$.form).querySelectorAll(elements);
-      return arrItems;
+    appendContent: function(node, content){
+     return Polymer.dom(node).innerHTML = content; 
     },
 
-    findLastElement: function(element){
-      var arrElements = this.findElements(element);
-      return arrElements[arrElements.length -1];
-    },
-
-    addTextToElement: function(text){
+    addTextToElement: function(element, text){
       var span = document.createElement('span');
-      Polymer.dom(span).innerHTML = ' ' + text;
-      return Polymer.dom(this.$.form).appendChild(span);
-    },
-
-    addBr: function(){
-      var br = document.createElement('br');
-      return Polymer.dom(this.$.form).appendChild(br);
+      this.appendContent(span,' ' + text);
+      return Polymer.dom(element).appendChild(span);
     },
 
     paperInputHandler: function(formElement){
-      var newElement = this.appendElement(formElement);
-      newElement.label = formElement.label;
-      newElement.name = formElement.inputName;
-      this.addBr();
-      return newElement;
+      return function(formElement){
+        this.addTextToElement(this.$.form,formElement.title);
+        if(formElement.description) this.addTextToElement(this.$.form,formElement.description);
+        var newElement = this.appendElement(this.$.form,formElement.type);
+        newElement.id = formElement.id;
+        newElement.label = formElement.label;
+        newElement.name = formElement.fieldName;
+        return newElement;
+      }
+    },
+
+    paperCheckboxHandler: function(formElement){
+      return function(formElement){
+        var options = formElement.options;
+        var elementType = formElement.type;
+        var arrElement = [];
+        this.addTextToElement(this.$.form,formElement.label);
+        
+        var i = 0, l = options.length;
+        for(i;i<l;i++){
+          var newElement = this.appendElement(this.$.form,elementType);
+          this.addTextToElement(this.$.form,formElement.options[i].name);
+          newElement.name = formElement.fieldName;
+          newElement.label = formElement.label;
+          arrElement.push(newElement);
+        }
+        return arrElement;
+      }
     },
 
     paperRadioHandler: function(formElement){
-      var options = formElement.options;
-      var elementType = formElement.polymerElement.parent;
-      var arrElement = [];
-      var i = 0, l = options.length;
-      for(i;i<l;i++){
-        var newElement = this.appendElement(formElement);
-        newElement.name = formElement.inputName;
-        arrElement.push(newElement);
-        this.addTextToElement(formElement.options[i].name);
-        this.addBr();
-      }
-      return arrElement;
-    },
-
-    elementHandler: function(formElement){
-      switch(formElement.polymerElement.parent){
-        case 'paper-input':
-          this.paperInputHandler(formElement);
-          break;
-        case 'paper-checkbox':
-          this.paperRadioHandler(formElement);
-          break;
-        case 'paper-radio-button':
-          this.paperRadioHandler(formElement);
-          break;
+      return function(formElement){
+        var arrElement = [];
+        var options = formElement.options;
+        var elementType = formElement.type;
+        var radioGroup = document.createElement('paper-radio-group');
+        
+        this.addTextToElement(this.$.form, formElement.label);
+        this.appendContent(radioGroup,document.createElement('paper-radio-button'));  
+        
+        var i = 0, l = options.length;
+        for(i;i<l;i++){
+          var newElement = this.appendElement(radioGroup,'paper-radio-button');
+          this.addTextToElement(radioGroup, options[i].name);
+          newElement.name = options[i].name;
+          newElement.label = options[i].name;
+          arrElement.push(newElement);
         }
+         return Polymer.dom(this.$.form).appendChild(radioGroup);
+      }
     },
 
     importComponent: function(formElement){
-      var path = this.bowerPath + formElement.polymerElement.parent + '/' + formElement.polymerElement.child + '.html';
-      this.importHref(path, function(e) {
-        this.elementHandler(formElement);
-      }, function(e) {
-        console.log(e);
-      });
+      switch(formElement.type){
+        
+        case 'paper-input':
+          var path = this.bowerPath + 'paper-input/paper-input.html';
+          var elementHandler = this.paperInputHandler().bind(this);
+          break;
+        case 'paper-textarea':
+          var path = this.bowerPath + 'paper-input/paper-textarea.html';
+          var elementHandler = this.paperInputHandler().bind(this);
+          break;
+        case 'paper-checkbox':
+          var path = this.bowerPath + 'paper-checkbox/paper-checkbox.html';
+          var elementHandler = this.paperCheckboxHandler().bind(this);
+          break;
+        case 'paper-radio-group':
+          var path = this.bowerPath + 'paper-radio-group/paper-radio-group.html';
+          var elementHandler = this.paperRadioHandler().bind(this);
+          break;
+        }
+
+        this.importHref(path, function(e) {
+          elementHandler(formElement);
+        }, function(e) {
+          console.log(e);
+        });
     },
 
     dataObserver: function(data){
@@ -96,5 +119,4 @@ Polymer({
         this.importComponent(formElements[i]);
       }
     }
-
 });
