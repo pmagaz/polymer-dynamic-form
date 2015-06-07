1f861952547f53b8e3f83fd48473a4968b482f80
Pablo Magaz <pmagaz@atsistemas.com>
2015-06-07 08:16:46 +0200

    Minor changes

--- dynamic-form.js
+++ dynamic-form.js
@@ -3,16 +3,6 @@ Polymer({
     is: "dynamic-form",
     
     properties: {
-      
-      data : {
-        type : Object,
-        notify : false,
-        observer : 'dataObserver'
-      },
-
-      bowerPath : {
-        type : String
-      },
 
       action : {
         type : String
@@ -21,12 +11,24 @@ Polymer({
       method : {
         type : String,
         value : 'POST'
+      },
+      
+      data : {
+        type : Object,
+        notify : false,
+        observer : 'dataObserver'
+      },
+
+      bowerPath : {
+        type : String,
+        value : '../../bower_components/'
       }
+
     },
 
     appendElement: function(node, element){
-      var newElement = document.createElement(element);
-      return Polymer.dom(node).appendChild(newElement);
+      var newComponent = document.createElement(element);
+      return Polymer.dom(node).appendChild(newComponent);
     },
 
     appendContent: function(node, content){
@@ -39,92 +41,95 @@ Polymer({
       return Polymer.dom(element).appendChild(span);
     },
 
-    paperInputHandler: function(formElement){
-      return function(formElement){
-        this.addTextToElement(this.$.form,formElement.title);
-        if(formElement.description) this.addTextToElement(this.$.form,formElement.description);
-        var newElement = this.appendElement(this.$.form,formElement.type);
-        newElement.id = formElement.id;
-        newElement.label = formElement.label;
-        newElement.name = formElement.fieldName;
-        return newElement;
+    paperInputHandler: function(formComponent){
+      return function(){
+        this.addTextToElement(this.$.form,formComponent.title);
+        if(formComponent.description) this.addTextToElement(this.$.form,formComponent.description);
+        var newComponent = this.appendElement(this.$.form,formComponent.type);
+        newComponent.id = formComponent.id;
+        newComponent.label = formComponent.label;
+        newComponent.name = formComponent.fieldName;
+        return newComponent;
       }
     },
 
-    paperCheckboxHandler: function(formElement){
-      return function(formElement){
-        var options = formElement.options;
-        var elementType = formElement.type;
+    paperCheckboxHandler: function(formComponent){
+      return function(){
+        var options = formComponent.options;
+        var elementType = formComponent.type;
         var arrElement = [];
-        this.addTextToElement(this.$.form,formElement.label);
+        this.addTextToElement(this.$.form,formComponent.label);
         
         var i = 0, l = options.length;
         for(i;i<l;i++){
-          var newElement = this.appendElement(this.$.form,elementType);
-          this.addTextToElement(this.$.form,formElement.options[i].name);
-          newElement.name = formElement.fieldName;
-          newElement.label = formElement.label;
-          arrElement.push(newElement);
+          var newComponent = this.appendElement(this.$.form,elementType);
+          this.addTextToElement(this.$.form,formComponent.options[i].name);
+          newComponent.name = formComponent.fieldName;
+          newComponent.label = formComponent.label;
+          arrElement.push(newComponent);
         }
         return arrElement;
       }
     },
 
-    paperRadioHandler: function(formElement){
-      return function(formElement){
+    paperRadioHandler: function(formComponent){
+      return function(){
         var arrElement = [];
-        var options = formElement.options;
-        var elementType = formElement.type;
+        var options = formComponent.options;
+        var elementType = formComponent.type;
         var radioGroup = document.createElement('paper-radio-group');
         
-        this.addTextToElement(this.$.form, formElement.label);
+        this.addTextToElement(this.$.form, formComponent.label);
         this.appendContent(radioGroup,document.createElement('paper-radio-button'));  
         
         var i = 0, l = options.length;
         for(i;i<l;i++){
-          var newElement = this.appendElement(radioGroup,'paper-radio-button');
+          var newComponent = this.appendElement(radioGroup,'paper-radio-button');
           this.addTextToElement(radioGroup, options[i].name);
-          newElement.name = options[i].name;
-          newElement.label = options[i].name;
-          arrElement.push(newElement);
+          newComponent.name = options[i].name;
+          newComponent.label = options[i].name;
+          arrElement.push(newComponent);
         }
          return Polymer.dom(this.$.form).appendChild(radioGroup);
       }
     },
 
-    importComponent: function(formElement){
-      switch(formElement.type){
-        
+    importComponent: function(path, callback){
+     this.importHref(path, function(e) {
+        callback();
+      }, function(e) {
+          throw new Error("Can't import "+ e.path[0].href);
+      });
+    },
+
+    importDependencies: function(formComponent){
+      switch(formComponent.type){
         case 'paper-input':
           var path = this.bowerPath + 'paper-input/paper-input.html';
-          var elementHandler = this.paperInputHandler().bind(this);
+          var callback = this.paperInputHandler(formComponent).bind(this);
           break;
         case 'paper-textarea':
           var path = this.bowerPath + 'paper-input/paper-textarea.html';
-          var elementHandler = this.paperInputHandler().bind(this);
+          var callback = this.paperInputHandler(formComponent).bind(this);
           break;
         case 'paper-checkbox':
           var path = this.bowerPath + 'paper-checkbox/paper-checkbox.html';
-          var elementHandler = this.paperCheckboxHandler().bind(this);
+          var callback = this.paperCheckboxHandler(formComponent).bind(this);
           break;
         case 'paper-radio-group':
           var path = this.bowerPath + 'paper-radio-group/paper-radio-group.html';
-          var elementHandler = this.paperRadioHandler().bind(this);
+          var callback = this.paperRadioHandler(formComponent).bind(this);
           break;
         }
 
-        this.importHref(path, function(e) {
-          elementHandler(formElement);
-        }, function(e) {
-          console.log(e);
-        });
+        this.importComponent(path, callback);
     },
 
     dataObserver: function(data){
-      var formElements = data.items;
-      var i = 0, l = formElements.length;
+      var formComponents = data.items;
+      var i = 0, l = formComponents.length;
       for(i;i<l;i++){
-        this.importComponent(formElements[i]);
+        this.importDependencies(formComponents[i]);
       }
     }
 });
